32ae491bdf0787de7fbdd9035736440abf8ea515
RatoGBM <80184495+RatoGBM@users.noreply.github.com>
2021-11-14 11:01:42 -0500

    The code.
    Run in Terminal and input the name in the first line.

---
+++ dialog_trainer.py
@@ -0,0 +1,30 @@
+#imports
+import pyttsx3 as tts
+
+#get the names of the characters
+name=input('name:')
+
+#load dialog
+dialog=0
+with open('scripts/dialogs/dialog_MovingOut.txt','r') as file:
+    dialog=file.readlines()
+dialog=[line.strip('\n') for line in dialog]
+#script
+enginne=tts.init()
+for line in dialog:
+    if line=='':
+        pass
+    elif len(line)<len(name) or line[:len(name)]!=name:
+        enginne.say(line[line.find(':')+1:])
+        print(line)
+        enginne.runAndWait()
+
+    else:
+        input('Press to continue.')
+        print('You: '+line[len(name):])
+        
+print("END Thank You")
+        
+    
+
+
